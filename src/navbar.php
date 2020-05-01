90b84431a2d71e60018554ef3713299fbdca1126
mehmet ali ipsuz <mehmetaliipsuz@hotmail.com>
2020-05-01 22:43:26 +0100

    navbar development

---
+++ src/navbar.php
@@ -0,0 +1,20 @@
+<link rel="stylesheet" type="text/css" href="../style/navbar.css">
+
+<nav class="navbar">
+    <a href="#" class="logo"><img src="../img/pages/navbar/icon.gif" height="25" width="25">Casebook</a>
+
+    <ul class="main-ul">
+        <li class="nav-item">Timeline</li>
+        
+        <li class="nav-item-dropdown">Create
+            <ul class="dropdown-ul">
+                <li class="nav-item-dropdown">InLine</li>
+                <li class="nav-item-dropdown">OEM</li>
+            </ul>
+        </li>
+        <li class="nav-item">Reporting</li>
+    </ul>        
+
+
+
+</nav>
